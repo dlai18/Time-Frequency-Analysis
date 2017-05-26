9f7be365baf13c035f4d8e358b9d7d42cd6f011f
Daniel Lai <daniel.lai5896@gmail.comm>
2017-05-26 15:19:44 -0700

    giving up on fourier

--- Code/discrete_time_fourier_transformation.py
+++ Code/discrete_time_fourier_transformation.py
@@ -5,19 +5,21 @@ import math
 srate = 1000
 speriod = 1/srate
 length_of_signal = 1500
-time = np.arange(0, length_of_signal, speriod)
+time = np.arange(0, length_of_signal, speriod) * speriod
 
 sine = 0.7 * np.sin(2 * math.pi * 50 * time) + np.sin(2 * math.pi * 120 * time)
 
 noise = sine + 2 * np.random.randn(len(time))
 
-plt.plot(1000 * time[0:50], noise[0:50])
+plt.plot(1000*time[0:49], noise[0:49])
 plt.show()
 
-fourier = fft(noise)
+fourier = np.fft.fft(noise)
 P2 = abs(fourier/length_of_signal)
 P1 = P2[0:length_of_signal/2 + 1]
-P1[2:end-1] = 2*P1[2:end-1]
-f = srate*[0:length_of_signal/2]/length_of_signal
+P1[1:-2] = 2 * P1[1:-2]
+
+f = srate * np.arange(0, length_of_signal/2 + 1)/length_of_signal
+
 plt.plot(f, P1)
 plt.show()
